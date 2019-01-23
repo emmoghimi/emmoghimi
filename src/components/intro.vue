0918bb1db558cefdd3d3736b99bff7f6652b6b26
unknown <emmoghimi@gmail.com>
2019-01-23 03:40:45 -0800

    Add new styles on intro

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -4,11 +4,11 @@
       <p class="app--home--intro--container--title">Hi <br> im Emad Moghimi <br> a Front-end web developer</p>
       <div class="app--home--intro--container--social-network d-flex flex-column">
           <ul class="app--home--intro--container--social-network--nav d-flex container nav flex-nowrap">
-            <li class="app--home--intro--container--social-network--nav--gitlab nav-item"><a href="" class="nav-link"><i class="fab fa-gitlab"></i></a></li>
-            <li class="app--home--intro--container--social-network--nav--github nav-item"><a href="" class="nav-link"><i class="fab fa-github"></i></a></li>
-            <li class="app--home--intro--container--social-network--nav--phone nav-item"><a href="" class="nav-link"><i class="fas fa-mobile-alt"></i></a></li>
-            <li class="app--home--intro--container--social-network--nav--twitter nav-item"><a href="" class="nav-link"><i class="fab fa-twitter"></i></a></li>
-            <li class="app--home--intro--container--social-network--nav--gmail nav-item"><a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1" class="nav-link"><i class="fas fa-envelope"></i></a></li>
+            <li class="app--home--intro--container--social-network--nav--gitlab nav-item"><a href="https://gitlab.com/emmoghimi" target="_blank" class="nav-link"><i class="fab fa-gitlab"></i></a></li>
+            <li class="app--home--intro--container--social-network--nav--github nav-item"><a href="http://github.com/emmoghimi" target="_blank" class="nav-link"><i class="fab fa-github"></i></a></li>
+            <li class="app--home--intro--container--social-network--nav--phone nav-item" @click="phoneInfo"><a href="#6" class="nav-link"><i class="fas fa-mobile-alt"></i></a></li>
+            <li class="app--home--intro--container--social-network--nav--twitter nav-item"><a href="http://twitter.com/emmoghimi" target="_blank" class="nav-link"><i class="fab fa-twitter"></i></a></li>
+            <li class="app--home--intro--container--social-network--nav--gmail nav-item"><a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1" target="_blank" class="nav-link"><i class="fas fa-envelope"></i></a></li>
           </ul>
       </div>
     </div>
@@ -19,6 +19,12 @@
 export default {
   data () {
     return {
+
+    }
+  },
+  methods:{
+    phoneInfo(){
+      alert("You can call me or send me message on telegram by this number \n +989372256142");
     }
   }
 }
